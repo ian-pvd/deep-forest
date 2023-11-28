5196d339fd5572046c36596d6dac02344921109e
Ian Del Giudice <ian.pvd@gmail.com>
2023-11-27 21:41:09 -0500

    wip add desktop menus styles and scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,47 @@ import { debounce, isMobile } from "./utils.js";
  */
 const toggleNavMenu = () => {
 	// Get menu elements.
-	const menuToggle = document.getElementById("site-header-menu-toggle");
-	const navbar = document.getElementById("site-header-nav");
+	const menuToggle = document.getElementById("site-header-nav-toggle");
+	const menu = document.getElementById("site-header-nav");
 
 	// Show & hide the menu aria values based on viewport width.
 	const toggleViewportAria = () => {
 		if (!isMobile()) {
 			menuToggle.setAttribute("aria-expanded", "true");
-			navbar.setAttribute("aria-hidden", "false");
+			menu.setAttribute("aria-hidden", "false");
 		} else {
 			menuToggle.setAttribute("aria-expanded", "false");
-			navbar.setAttribute("aria-hidden", "true");
+			menu.setAttribute("aria-hidden", "true");
+		}
+	};
+	// Set initial states for desktop.
+	toggleViewportAria();
+	// Toggle menu display on viewport resize.
+	window.addEventListener("resize", debounce(toggleViewportAria, 150));
+
+	// Show & hide the mobile menu on click.
+	// TODO: Toggle doesn't close now.
+	menuToggle.addEventListener("click", function () {
+		const expanded =
+			menuToggle.getAttribute("aria-expanded") === "true" || "false";
+		menuToggle.setAttribute("aria-expanded", !expanded);
+		menu.setAttribute("aria-hidden", expanded);
+	});
+};
+
+const toggleSocialMenu = () => {
+	// Get menu elements.
+	const menuToggle = document.getElementById("social-links-toggle");
+	const menu = document.getElementById("social-links-menu");
+
+	// Show & hide the menu aria values based on viewport width.
+	const toggleViewportAria = () => {
+		if (!isMobile()) {
+			menuToggle.setAttribute("aria-expanded", "false");
+			menu.setAttribute("aria-hidden", "true");
+		} else {
+			menuToggle.setAttribute("aria-expanded", "true");
+			menu.setAttribute("aria-hidden", "false");
 		}
 	};
 	// Set initial states for desktop.
@@ -34,13 +64,14 @@ const toggleNavMenu = () => {
 	// Show & hide the mobile menu on click.
 	menuToggle.addEventListener("click", function () {
 		const expanded =
-			menuToggle.getAttribute("aria-expanded") === "true" || false;
+			menuToggle.getAttribute("aria-expanded") === "true" || "false";
 		menuToggle.setAttribute("aria-expanded", !expanded);
-		navbar.setAttribute("aria-hidden", String(expanded));
+		menu.setAttribute("aria-hidden", expanded);
 	});
 };
 
 /* Initialize Scripts */
 document.addEventListener("DOMContentLoaded", function () {
 	toggleNavMenu();
+	toggleSocialMenu();
 });
