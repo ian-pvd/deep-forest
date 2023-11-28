61dc60b26ed258236ba493eccd1c8d7624be211a
Ian Del Giudice <ian.pvd@gmail.com>
2023-11-28 15:44:02 -0500

    style desktop header nav and mobile QA

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,12 @@ const toggleNavMenu = () => {
 	const menuToggle = document.getElementById("site-header-nav-toggle");
 	const menu = document.getElementById("site-header-nav");
 
+	// DEV
+	// menuToggle.setAttribute("aria-expanded", "true");
+	// menu.setAttribute("aria-hidden", "false");
+	// document.body.setAttribute("data-nav-hidden", "false");
+	// return;
+
 	// Show & hide the menu aria values based on viewport width.
 	const toggleViewportAria = () => {
 		if (!isMobile()) {
